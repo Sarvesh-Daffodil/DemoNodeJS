0e81c64fe377de4a8385ecb595fcd384e82f1de4
Sarvesh Singh <sarvesh.singh@daffodilsw.com>
2020-04-24 01:20:33 +0530

    Added Section4

---
+++ Section4.md
@@ -0,0 +1,9 @@
+# Section 4 - NodeJS coding - (15 - 20 mins) #
+
+# 8 (a) is a trivial example of callback hell
+
+* Refactor the code for the better, using any techniques you are aware of
+* Place code in load_contents.js
+* Take path as an argument
+
+# Refer load_contents.js
---
+++ load_contents.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const path = process.argv[2];
+const util = require('util');
+
+const getFileContents = async (path) => {
+   try {
+       let exists, stats, buffer;
+       
+       const promisifyFsExists = util.promisify(fs.exists);
+       const exists = await promisifyFsExists(path);
+       
+       if (!exists) {
+           throw Error("File does not exist");;
+       }
+       console.log('exists');
+       try {
+           const promisifyFsStat = util.promisify(fs.stat);
+           stats = await promisifyFsStat(path);
+           console.log(stats);
+       } catch (err) {
+           throw Error('Error trying to get stats');
+       }
+       
+       if (stats.size <= 0) {
+           throw Error('File exists but there is no content');
+       }
+       try {
+           const promisifyFsReadFile = util.promisify(fs.stat)
+           buffer = await promisifyFsReadFile(path);
+           console.info('File was found and the contents were loaded');
+       }
+       catch (err) {
+           throw Error('Error trying to get stats');
+       }
+   } catch (err) {
+       console.error(`There was an error getting contents for ${path}`, err);
+   }
+}
+
+getFileContents(path);
